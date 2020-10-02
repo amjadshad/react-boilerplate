5bb100baf2153d22885990104966c383ee59a899
irvingangulo <irving@eventops.com>
2020-10-02 12:31:29 -0400

    Install libraries, define Router and layout
    **This commit:**
    
    - Set up React app with typescript
    - Install node-sass-chokidar, react-router-dom, classnames, @types/react-router-dom, axios
    - Define MainLayout and Router
    - Remove unused files from create-react-app
    - Add /src structure to include layouts, router, pages, components, and API. Define these.
    - Add Prettier
    
    **Why?**
    To have a boilerplate for React App using typescript so that in the future it is easier to start a project.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Header.scss'
+
+export const Header: React.FC = () => {
+  return (
+    <>
+      <div className="Header">
+        <div>
+          <span>Eventops header, Logo, etc</span>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/pages/BrandedPageEditor/index.js
@@ -0,0 +1,7 @@
+import React, { Component, useRef, useEffect, useState } from 'react'
+
+const BrandedPageEditor = (props) => {
+  return <div>BrandedPageEditor</div>
+}
+
+export default BrandedPageEditor
