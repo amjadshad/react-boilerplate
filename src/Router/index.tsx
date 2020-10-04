e15649bcbfd49d51fff6cfa8cad9ad53b222a23f
irvingangulo <irving@eventops.com>
2020-10-04 15:55:15 -0400

    Editor works in version

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
-import { BrandedPageEditor } from "../pages/BrandedPageEditor";
+import BrandedPageEditor from "../pages/BrandedPageEditor";
 import { MainLayout } from "../Layouts/MainLayout";
 import { BrandedPageDisplay } from "../pages/BrandedPageDisplay";
 
--- src/pages/BrandedPageEditor/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useRef } from "react";
-
-import EmailEditor from "react-email-editor";
-
-export const BrandedPageEditor: React.FC = (props) => {
-  return <>This is the BrandedPageEditor</>;
-};
